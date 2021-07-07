7ef6b6d1a352351064500ccedecf61d6baf4909d
ulvi macid <ulvizm@code.edu.az>
2021-07-07 14:13:31 +0400

    add controller && dto

--- top-api/src/product/product.controller.ts
+++ top-api/src/product/product.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { ProductModel } from './product.model';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+    @Post("create")
+    async create(@Body()dto:Omit<ProductModel,"_id">){
+
+    }
+
+    @Get(":id")
+    async get(@Param("id") id:string){
+
+    }
+    @Delete(":id")
+    async delete(@Param("id") id:string){
+
+    }
+    Patch(":id")
+    async patch(@Param("id")id:string,@Body()dto:ProductModel){
+
+    }
+}
